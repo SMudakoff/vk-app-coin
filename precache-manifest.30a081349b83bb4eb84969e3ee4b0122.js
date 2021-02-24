f574d5359c7075d42e8e11d2f75cc8912fa92f20
Ushakov Sergey <serega_twitch@mail.ru>
2021-02-25 01:49:01 +0300

    Updates

---
+++ precache-manifest.30a081349b83bb4eb84969e3ee4b0122.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d16e69ec957c8d76bd71bb05e12c21",
+    "url": "./index.html"
+  },
+  {
+    "revision": "23068513a8989e661eec",
+    "url": "./static/css/2.cdee455b.chunk.css"
+  },
+  {
+    "revision": "a0c327baabf1f8352a63",
+    "url": "./static/css/main.8b3c5f38.chunk.css"
+  },
+  {
+    "revision": "23068513a8989e661eec",
+    "url": "./static/js/2.dc3bd2cb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.dc3bd2cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0c327baabf1f8352a63",
+    "url": "./static/js/main.37c4a9a1.chunk.js"
+  },
+  {
+    "revision": "b3f14a98a4b3b1d28458",
+    "url": "./static/js/runtime-main.176c76de.js"
+  },
+  {
+    "revision": "b528eaf852da05eb9fbb826b13d854d5",
+    "url": "./static/media/roll.b528eaf8.svg"
+  }
+]);
